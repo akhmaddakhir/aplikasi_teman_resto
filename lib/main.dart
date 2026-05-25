a3a3af29cef84e62e3e970d663dfd752825763d9
Mulyadisapusapu <kairulkumar50@gmail.com>
2026-05-25 19:50:01 +0700

    integrate Firebase dependencies and initialize Firebase in main; update plugin registration for cloud_firestore, firebase_auth, and firebase_core

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 // Auth
 import 'pages/auth/splashscreen_page.dart';
@@ -53,7 +55,11 @@ import 'pages/navigate/navigate_page.dart';
 // Widgets
 import 'widgets/bottom_navbar.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
